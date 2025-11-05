6162560866abc1151cc70e12348a68da3c917439
Carmela Sotuyo <carmela.sotuyo@lightit.io>
2025-11-05 11:38:39 -0300

    feat(CU-86dy72t5r): Appointment Management - add authorization policy for appointments

--- src/Appointments/App/Controllers/DeleteAppointmentController.php
+++ src/Appointments/App/Controllers/DeleteAppointmentController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Lightit\Appointments\App\Controllers;
 
 use Dedoc\Scramble\Attributes\Group;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\JsonResponse;
 use Lightit\Appointments\Domain\Actions\CancelAppointmentAction;
 use Lightit\Appointments\Domain\Models\Appointment;
@@ -12,8 +13,12 @@ use Lightit\Appointments\Domain\Models\Appointment;
 #[Group('Appointments')]
 final readonly class DeleteAppointmentController
 {
+    use AuthorizesRequests;
+
     public function __invoke(Appointment $appointment, CancelAppointmentAction $action): JsonResponse
     {
+        $this->authorize('delete', $appointment);
+
         $action->execute($appointment);
 
         return response()->json([
