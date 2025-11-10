59072d2f0e8584fb99bb0573aacb0f4b180a09e3
Carmela Sotuyo <carmela.sotuyo@lightit.io>
2025-11-10 14:22:04 -0300

    feat(CU-86dy72tfp): Email Notifications - implement automated phpdoc to all models

--- src/Appointments/Domain/Models/Appointment.php
+++ src/Appointments/Domain/Models/Appointment.php
@@ -15,16 +15,36 @@ use Lightit\Doctors\Domain\Models\Doctor;
 use Lightit\Users\Domain\Models\User;
 
 /**
- * @property int             $id
- * @property int             $doctor_id
- * @property int             $clinic_id
- * @property int             $user_id
- * @property CarbonImmutable $starts_at
- * @property CarbonImmutable $ends_at
+ * @property int                  $id
+ * @property int                  $doctor_id
+ * @property int                  $user_id
+ * @property int                  $clinic_id
+ * @property CarbonImmutable      $starts_at
+ * @property CarbonImmutable      $ends_at
+ * @property CarbonImmutable|null $deleted_at
+ * @property CarbonImmutable|null $created_at
+ * @property CarbonImmutable|null $updated_at
+ * @property-read Clinic $clinic
+ * @property-read Doctor $doctor
+ * @property-read User $user
  *
- * @property-read Doctor     $doctor
- * @property-read Clinic     $clinic
- * @property-read User       $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment whereClinicId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment whereDoctorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment whereEndsAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment whereStartsAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment withTrashed(bool $withTrashed = true)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Appointment withoutTrashed()
+ *
+ * @mixin \Eloquent
  */
 #[UsePolicy(AppointmentPolicy::class)]
 class Appointment extends Model
